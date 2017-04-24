e7ad405456d097f68514ec29c3532d088e36a3e9
renzon <renzon@gmail.com>
2017-04-24 20:30:02 -0300

    Atualizando Baralho com naipes unicode

--- baralho.py
+++ baralho.py
@@ -4,9 +4,10 @@ from collections import namedtuple
 
 Carta = namedtuple('Carta', 'valor naipe')
 
+
 class Baralho:
-    valores = [str(n) for n in range(2, 11)] + list('JQKA')
-    naipes = 'paus ouros copas espadas'.split()
+    valores = '1 2 3 4 5 6 7 8 9 10 J Q K A'.split()
+    naipes = '♣ ♢ ♡ ♠'.split()
 
     def __init__(self):
         self.cartas = [Carta(v, n) for n in self.naipes for v in self.valores]
@@ -16,7 +17,3 @@ class Baralho:
 
     def __getitem__(self, pos):
         return self.cartas[pos]
-
-
-
-
