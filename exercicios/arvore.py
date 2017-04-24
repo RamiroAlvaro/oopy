c9f0c2e56fbb83147fd0db6658c8a381b1eb4db1
renzon <renzon@gmail.com>
2017-04-24 20:40:57 -0300

    Arvore corrigida para coincidir com árvore binária de busca

--- exercicios/arvore.py
+++ exercicios/arvore.py
@@ -1,11 +1,11 @@
 class Arvore:
     '''
-        >>> for noh in Arvore(0, Arvore(-2, Arvore(-4), Arvore(-3)), Arvore(
+        >>> for noh in Arvore(0, Arvore(-2, Arvore(-4), Arvore(-1)), Arvore(
         ... 10)):
         ...     print(noh)
         -4
-        -1
         -2
+        -1
         0
         10
         
@@ -15,9 +15,3 @@ class Arvore:
         self.valor = valor
         self.esquerda = esquerda
         self.direita = direita
-
-
-
-        #           0
-        #    -2            10
-        #  -4   -1
